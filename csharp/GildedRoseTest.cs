6e141f1d5626136d2968f49fc638ee28fcc4f7db
Michal Bertko <michal.bertko@gmail.com>
2018-06-01 18:25:11 +0200

    AgedBrie tests added

--- csharp/GildedRoseTest.cs
+++ csharp/GildedRoseTest.cs
@@ -1,5 +1,6 @@
 ﻿using NUnit.Framework;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace csharp
 {
@@ -8,6 +9,7 @@ namespace csharp
     {
         private const string sulfuras = "Sulfuras, Hand of Ragnaros";
         private const string basicItem = "Elixir of the Mongoose";
+        private const string agedBrie = "Aged Brie";
 
         private IList<Item> items;
 
@@ -61,14 +63,15 @@ namespace csharp
         [TestCase(0)]
         [TestCase(15)]
         [TestCase(-5)]
-        public void BasicItem_UpdateQuality_SellInDecreasesCorrectly(int initSellIn)
+        public void AllItems_UpdateQuality_SellInDecreasesCorrectly(int initSellIn)
         {
             items.Add(new Item { Name = basicItem, SellIn = initSellIn, Quality = 20 });
+            items.Add(new Item { Name = agedBrie, SellIn = initSellIn, Quality = 10 });
             GildedRose app = new GildedRose(items);
 
             app.UpdateQuality();
 
-            Assert.That(items[0].SellIn, Is.EqualTo(initSellIn - 1));
+            Assert.That(items.Select(item => item.SellIn), Is.All.EqualTo(initSellIn - 1));
         }
 
         [Test]
@@ -87,6 +90,39 @@ namespace csharp
             }
 
             Assert.That(items[0].Quality, Is.EqualTo(expectedQuality));
+        }
+
+        [Test]
+        [TestCase(10, 25)]
+        [TestCase(1, 29)]
+        [TestCase(-5, 30)]
+        public void AgedBrie_UpdateQuality_QualityIncreasesCorrectly(int initSellIn, int expectedQuality)
+        {
+            int initQuality = 20;
+            items.Add(new Item { Name = agedBrie, SellIn = initSellIn, Quality = initQuality });
+            GildedRose app = new GildedRose(items);
+
+            for (int i = 0; i < 5; i++)
+            {
+                app.UpdateQuality();
+            }
+
+            Assert.That(items[0].Quality, Is.EqualTo(expectedQuality));
+        }
+
+        [Test]
+        [TestCase(50)]
+        [TestCase(20)]
+        [TestCase(48)]
+        [TestCase(49)]
+        public void AgedBrie_UpdateQuality_QualityNotIncreasedAboveLimit(int initQuality)
+        {
+            items.Add(new Item { Name = agedBrie, SellIn = -5, Quality = initQuality });
+            GildedRose app = new GildedRose(items);
+
+            app.UpdateQuality();
+
+            Assert.That(items[0].Quality, Is.Not.GreaterThan(50));
         }
     }
 }
